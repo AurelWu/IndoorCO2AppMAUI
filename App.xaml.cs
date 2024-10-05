9223c9e1f67805954b86b374b34488cbb389740a
Aurel Wünsch <aurel.wuensch@prognos.com>
2024-10-05 11:47:03 +0200

    added info about Rangeslider Control

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,10 @@ namespace IndoorCO2App_Multiplatform
         public App()
         {
 
-
+            //currently it uses Syncfusion for the Rangeslider, which requires a license which is free as long as your revenue is below some millions
+            //however including the serial key directly is not allowed so the file containing the key is not included. 
+            //You need to either create your own account and your own key or replace the RangeSlider control.
+            //It still works without license but shows an Info Popup at start of the App
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(SerialKey.SyncFusionKey);
             InitializeComponent();
 
